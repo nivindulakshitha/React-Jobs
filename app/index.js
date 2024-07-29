fb3bbdcd8ec98f28cb4b7d8110ecf6de0515b610
nivindulakshitha <nivindulakshitha@gamil.com>
2024-07-29 23:17:21 +0530

    chore: Update component imports in Home screen
    The component imports in the Home screen have been updated to reflect changes in the file structure. The Nearbyjobs component has been renamed to NearbyJobs, and the Popularjobs component has been added.

--- app/index.js
+++ app/index.js
@@ -3,17 +3,31 @@ import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, images, SIZES, FONTS } from '../constants';
-import { Nearbyjobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
-
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen options={{
                 headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.search} dimension="100%" />
+                ),
             }} />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
