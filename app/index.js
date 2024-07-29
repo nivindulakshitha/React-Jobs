d4ba1cbd8410b1d8aba57003271b4013664feaa0
nivindulakshitha <nivindulakshitha@gamil.com>
2024-07-29 23:26:01 +0530

    chore: Update ScreenHeaderBtn to use profile image

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ const Home = () => {
                     <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
                 ),
                 headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={icons.search} dimension="100%" />
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
                 ),
             }} />
 
--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { TouchableOpacity, Image } from 'react-native'
 
 import styles from './screenheader.style'
 
-const ScreenHeaderBtn = () => {
-  return (
-    <View>
-      <Text>ScreenHeaderBtn</Text>
-    </View>
-  )
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+	return (
+		<TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+			<Image
+				source={iconUrl}
+				resizeMode='cover'
+				style={styles.btnImg(dimension)}
+			/>
+		</TouchableOpacity>
+	)
 }
 
 export default ScreenHeaderBtn
