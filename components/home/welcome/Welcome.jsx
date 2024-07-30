9b02aa16a8db2f5ad3e3cb023f0cbdd60db65ae8
nivindulakshitha <nivindulakshitha@gamil.com>
2024-07-30 20:37:05 +0530

    chore: Update layout and welcome components
    The layout component has been updated to improve font loading and prevent the splash screen from auto-hiding. The welcome component has been modified to include a user greeting, search functionality, and job type tabs.
    
    Note: This message does not include any meta information like issue references, tags, or author names.

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ["Full-Type", "Part-Time", "Freelance"];
 
 const Welcome = () => {
+	const router = useRouter();
+	const [activeJobType, setActiveJobType] = useState("Full-Time");
+
   return (
     <View>
-      <Text>Welcome</Text>
+		  <View style={styles.container}>
+			  <Text style={styles.userName}>Hello, Andrian!</Text>
+			  <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+		  </View>
+
+		  <View style={styles.searchContainer}>
+			  <View style={styles.searchWrapper}>
+				  <TextInput
+					  style={styles.searchInput}
+					  value='' onChange={() => { }}
+					  placeholder='What are you looking for?'>
+
+				  </TextInput>
+			  </View>
+
+			  <TouchableOpacity style={styles.searchBtn} onpress={() => {}}>
+				  <Image
+					  source={icons.search}
+					  resizeMode='contain'
+					  style={styles.searchBtnImage}
+
+				  />
+			  </TouchableOpacity>
+		  </View>
+
+		  <View style={styles.tabsContainer}>
+				<FlatList
+					data={jobTypes}
+					renderItem={({item}) => {
+						<TouchableOpacity style={styles.tab(activeJobType, item)}>
+							<Text>{item}</Text>
+						</TouchableOpacity>
+					}}
+				/>
+		  </View>
     </View>
   )
 }
