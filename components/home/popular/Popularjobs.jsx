37c423665a1ba91707461ccb981e674a384ee271
nivindulakshitha <nivindulakshitha@gmail.com>
2024-07-30 21:41:31 +0530

    chore: Update Popularjobs component layout and header
    The Popularjobs component layout has been updated to include a container and a header section. The header section now displays the title "Popular Jobs" and a "Show all" button.
    
    Note: This message does not include any meta information like issue references, tags, or author names.

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerTitle}>Popular Jobs</Text>
+				<TouchableOpacity>
+					<Text style={styles.headerBtn}>Show all</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	)
 }
 
 export default Popularjobs
