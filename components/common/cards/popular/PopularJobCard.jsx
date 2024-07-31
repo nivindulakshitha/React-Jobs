554c26101a1b8c3661adae4c6b1c19cc5b623460
nivindulakshitha <nivindulakshitha@gmail.com>
2024-07-31 20:33:06 +0530

    refactor: Update PopularJobCard component to include logo, employer name, and job details

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -3,19 +3,31 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
 
-const PopularJobCard = ({item, selecetdJob, handleCardPress}) => {
-  return (
-    <TouchableOpacity style={styles.container(selecetdJob, item)}>
-      <TouchableOpacity style={styles.logoContainer(selecetdJob, item)}>
-        <Image
-          source={{url: item.employer_logo}}
-          resizeMode='contain'
-          style={styles.logoImage}
-        />
-      </TouchableOpacity>
-      <Text>{item.employer_name}</Text>
-    </TouchableOpacity>
-  )
+const PopularJobCard = ({ item, selecetdJob, handleCardPress }) => {
+	return (
+		<TouchableOpacity style={styles.container(selecetdJob, item)}>
+
+			<TouchableOpacity style={styles.logoContainer(selecetdJob, item)}>
+				<Image
+					source={{ url: item.employer_logo }}
+					resizeMode='contain'
+					style={styles.logoImage}
+				/>
+			</TouchableOpacity>
+
+			<Text style={styles.companyName} numberOfLines={1}>
+				{item.employer_name}
+			</Text>
+
+			<View style={styles.infoContainer}>
+				<Text style={styles.jobName(selecetdJob, item)} numberOfLines={1}>
+					{item.job_title}
+				</Text>
+
+				<Text style={styles.location}>{item.job_country}</Text>
+			</View>
+		</TouchableOpacity>
+	)
 }
 
 export default PopularJobCard
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -20,6 +20,7 @@ const useFetch = (endpoint, query) => {
         try {
             setIsLoading(true);
             const response = await axios.request(options);
+            console.log(response.data)
             setData(response.data);
             setIsLoading(false);
         } catch (error) {
