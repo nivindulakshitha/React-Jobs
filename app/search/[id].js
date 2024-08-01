f26125df8a84826ea0484b9590f22c384253b902
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-01 21:45:03 +0530

    ```text refactor: Update expo-router dependency to useLocalSearchParams
    The expo-router dependency in the [id].js file has been updated to use the useLocalSearchParams hook instead of useSearchParams. This change aligns with recent updates to the expo-router library and ensures compatibility with the latest version.

--- app/search/[id].js
+++ app/search/[id].js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
-import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { Stack, useRouter, useLocalSearchParams } from 'expo-router'
 import { Text, SafeAreaView } from 'react-native'
 import axios from 'axios'
 
@@ -9,7 +9,7 @@ import { COLORS, icons, SIZES } from '../../constants'
 import styles from '../../styles/search'
 
 const JobSearch = () => {
-    const params = useSearchParams();
+    const params = useLocalSearchParams();
     const router = useRouter()
 
     const [searchResult, setSearchResult] = useState([]);
