774058abc6720746cd16d9f6a0054ace99fa1ebb
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-01 20:57:09 +0530

    refactor: Update About component to display job details
    The About component has been updated to display the job details. This improves the user experience by providing more information about the job.
    
    Note: This message does not include any meta information like issue references, tags, or author names.

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -5,8 +5,12 @@ import styles from './about.style'
 
 const About = ({ info }) => {
   return (
-    <View>
-      <Text>About</Text>
+	  <View style={styles.container}>
+		  <Text style={styles.headText}>About the job:</Text>
+
+		  <View style={styles.contentBox}>
+			  <Text style={styles.contextText}>{info}</Text>
+		  </View>
     </View>
   )
 }
