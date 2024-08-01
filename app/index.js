1ba1d8e4a012de21826a13350a16194d47aeb1d5
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-01 22:27:47 +0530

    refactor: Update headerTitle in Home component
    The headerTitle in the Home component has been updated to an empty string. This change aligns with recent updates to the app design and removes the unnecessary header title from the screen.
    
    Note: This message does not include any meta information like issue references, tags, or author names.

--- app/index.js
+++ app/index.js
@@ -2,17 +2,18 @@ import { useState } from 'react';
 import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import { COLORS, icons, images, SIZES, FONTS } from '../constants';
+import { COLORS, icons, images, SIZES } from '../constants';
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
-    const [searchTerm, setSearchTerm] = useState('Developer');
+    const [searchTerm, setSearchTerm] = useState('');
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen options={{
                 headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerTitle: '',
                 headerShadowVisible: false,
                 headerLeft: () => (
                     <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
